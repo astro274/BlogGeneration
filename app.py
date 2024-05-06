d2c99060fac959c0e0e309a8faded6b94a1d7f81
astro274 <adnanmohamed922@gmail.com>
2024-05-06 20:32:57 +0530

    Completed building the pipeline

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
 import streamlit as st
 from langchain.prompts import PromptTemplate
-from langchain.llms import CTransformers
+from langchain_community.llms import CTransformers
 
 def getResponse(input_text, no_words, blog_style):
     """A Function to get the response from Llama 2"""
-    llm = CTransformers(model='D:\Career\Projects\BlogGeneration\models\llama-2-7b-chat.ggmlv3.q8_0.bin')
+
+    # Declaring the model
+    # Download the model from HuggingFace, place it in a folder, and copy paste the path in place of the path here
+    llm = CTransformers(model = 'models\llama-2-7b-chat.ggmlv3.q8_0.bin',
+                        model_type = "llama",
+                        config = {'max_new_tokens':256,
+                                'temperature':0.01})
+    
+    # Prompt template
+    template = """
+    Write a blog for {blog_style} job profile for a topic {input_text} within {no_words}.
+            """
+    prompt = PromptTemplate(input_variables=["blog_style", "input_text", "no_words"],
+                            template=template)
+    
+    # Generating the response from Llama 2
+    response = llm(prompt.format(blog_style=blog_style, input_text=input_text, no_words=no_words))
+    print(response)
+
+    return response
+
 
 st.set_page_config(page_title="Blog Generator", 
                    layout="centered",
                    initial_sidebar_state="collapsed")
 
-st.header("Generating Blogs using Llama 2")
+st.header("Blog Generator")
 
 input_text = st.text_input("Enter Blog Topic")
 
